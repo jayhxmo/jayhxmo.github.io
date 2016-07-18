8d050fc25029001557550bc08b4ebbf36c89e0a4
Jay Mo <jayhxmo@gmail.com>
2016-07-18 08:35:41 -0700

    Update scroll

--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,7 @@
+console.log(-$('#sidr').height() * 1.25);
 $('body').plusAnchor({
     easing: 'easeInOutExpo',
-    offsetTop: -$('#sidr').height(),
+    offsetTop: -$('#sidr').height() * 1.25,
     speed: 1000,
     onInit: null,
     onSlide: null,
